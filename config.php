ff1d292bfa0b062d1b7996181613eb6a5340d278
Hector Dufau <contato@hectordufau.com.br>
2021-02-05 20:15:02 -0300

    WIP #8 Config homolog

--- config.php
+++ config.php
@@ -13,6 +13,18 @@ if (getenv('ENV') == 'production')
     mysql_query('SET character_set_client=utf8');
     mysql_query('SET character_set_results=utf8');
 }
+if (getenv('ENV') == 'homolog') {
+    $hostname_conexao = "opmy0031.servidorwebfacil.com";
+    $database_conexao = "megapedigree_homolog";
+    $username_conexao = "megap_homolog";
+    $password_conexao = "H0m0l0g@"; //Companheiro3@10
+    $conexao = mysql_pconnect($hostname_conexao, $username_conexao, $password_conexao) or die(mysql_error());
+    mysql_select_db($database_conexao, $conexao);
+    mysql_query("SET NAMES 'utf8'");
+    mysql_query('SET character_set_connection=utf8');
+    mysql_query('SET character_set_client=utf8');
+    mysql_query('SET character_set_results=utf8');
+}
 else if (getenv('ENV') == 'development')
 {
     $hostname_conexao = "192.168.33.1";
